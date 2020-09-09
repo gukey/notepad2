gukey/notepad2

commit 0ae4308b54728d4a9dc0536fb9f88106aaa354ba

Optimize code folding.

scintilla/lexers/LexCPP.cxx
@@ -1245,12 +1245,9 @@ static void FoldCppDoc(Sci_PositionU startPos, Sci_Position length, int initStyl
 		const bool atEOL = (ch == '\r' && chNext != '\n') || (ch == '\n');
 
 		if (foldComment && atEOL && IsCommentLine(lineCurrent)) {
-			if (!IsCommentLine(lineCurrent - 1) && IsCommentLine(lineCurrent + 1))
-				levelNext++;
-			else if (IsCommentLine(lineCurrent - 1) && !IsCommentLine(lineCurrent + 1))
-				levelNext--;
+			levelNext += IsCommentLine(lineCurrent + 1) - IsCommentLine(lineCurrent - 1);
 		}
-		if (foldComment && IsStreamCommentStyle(style) && !IsCommentLine(lineCurrent)) {
+		if (foldComment && IsStreamCommentStyle(style)) {
 			if (!IsStreamCommentStyle(stylePrev)) {
 				levelNext++;
 			} else if (!IsStreamCommentStyle(styleNext) && !atEOL) {
@@ -1279,41 +1276,25 @@ static void FoldCppDoc(Sci_PositionU startPos, Sci_Position length, int initStyl
 		}
 
 		if (atEOL && IsUsingLine(lineCurrent)) {
-			if (!IsUsingLine(lineCurrent - 1) && IsUsingLine(lineCurrent + 1))
-				levelNext++;
-			else if (IsUsingLine(lineCurrent - 1) && !IsUsingLine(lineCurrent + 1))
-				levelNext--;
+			levelNext += IsUsingLine(lineCurrent + 1) - IsUsingLine(lineCurrent - 1);
 		}
-		//if (hasPreprocessor && atEOL && IsPropertyLine(lineCurrent)) {
-		//	if(!IsPropertyLine(lineCurrent-1) && IsPropertyLine(lineCurrent+1))
-		//		levelNext++;
-		//	else if(IsPropertyLine(lineCurrent-1) && !IsPropertyLine(lineCurrent+1))
-		//		levelNext--;
-		//}
-		if (hasPreprocessor && atEOL && IsIncludeLine(lineCurrent)) {
-			if (!IsIncludeLine(lineCurrent - 1) && IsIncludeLine(lineCurrent + 1))
-				levelNext++;
-			else if (IsIncludeLine(lineCurrent - 1) && !IsIncludeLine(lineCurrent + 1))
-				levelNext--;
-		}
-		if (hasPreprocessor && atEOL && IsDefineLine(lineCurrent)) {
-			if (!IsDefineLine(lineCurrent - 1) && IsDefineLine(lineCurrent + 1))
-				levelNext++;
-			else if (IsDefineLine(lineCurrent - 1) && !IsDefineLine(lineCurrent + 1))
-				levelNext--;
-		}
-		if (hasPreprocessor && atEOL && IsUnDefLine(lineCurrent)) {
-			if (!IsUnDefLine(lineCurrent - 1) && IsUnDefLine(lineCurrent + 1))
-				levelNext++;
-			else if (IsUnDefLine(lineCurrent - 1) && !IsUnDefLine(lineCurrent + 1))
-				levelNext--;
+		if (hasPreprocessor && atEOL) {
+			if (IsIncludeLine(lineCurrent)) {
+				levelNext += IsIncludeLine(lineCurrent + 1) - IsIncludeLine(lineCurrent - 1);
+			}
+			else if (IsDefineLine(lineCurrent)) {
+				levelNext += IsDefineLine(lineCurrent + 1) - IsDefineLine(lineCurrent - 1);
+			}
+			else if (IsUnDefLine(lineCurrent)) {
+				levelNext += IsUnDefLine(lineCurrent + 1) - IsUnDefLine(lineCurrent - 1);
+			}
+			//else if (IsPropertyLine(lineCurrent)) {
+			//	levelNext += IsPropertyLine(lineCurrent + 1) - IsPropertyLine(lineCurrent - 1);
+			//}
 		}
 
-		if (atEOL && !IsStreamCommentStyle(style) && IsBackslashLine(lineCurrent, styler) && !IsBackslashLine(lineCurrent - 1, styler)) {
-			levelNext++;
-		}
-		if (atEOL && !IsStreamCommentStyle(style) && !IsBackslashLine(lineCurrent, styler) && IsBackslashLine(lineCurrent - 1, styler)) {
-			levelNext--;
+		if (atEOL && !IsStreamCommentStyle(style)) {
+			levelNext += IsBackslashLine(lineCurrent, styler) - IsBackslashLine(lineCurrent - 1, styler);
 		}
 		if (atEOL && !(hasPreprocessor && IsCppInDefine(i, styler)) && IsOpenBraceLine(lineCurrent + 1, styler)) {
 			levelNext++;

scintilla/lexers/LexAsm.cxx
@@ -391,29 +391,16 @@ static void FoldAsmDoc(Sci_PositionU startPos, Sci_Position length, int initStyl
 			}
 		}
 		if (foldComment && atEOL && IsCommentLine(lineCurrent)) {
-			if (!IsCommentLine(lineCurrent - 1) && IsCommentLine(lineCurrent + 1))
-				levelNext++;
-			else if (IsCommentLine(lineCurrent - 1) && !IsCommentLine(lineCurrent + 1))
-				levelNext--;
+			levelNext += IsCommentLine(lineCurrent + 1) - IsCommentLine(lineCurrent - 1);
 		}
 		if (foldPreprocessor && atEOL && IsAsmDefineLine(lineCurrent, styler)) {
-			if (!IsAsmDefineLine(lineCurrent - 1, styler) && IsAsmDefineLine(lineCurrent + 1, styler))
-				levelNext++;
-			else if (IsAsmDefineLine(lineCurrent - 1, styler) && !IsAsmDefineLine(lineCurrent + 1, styler))
-				levelNext--;
+			levelNext += IsAsmDefineLine(lineCurrent + 1, styler) - IsAsmDefineLine(lineCurrent - 1, styler);
 		}
-		if (atEOL && IsBackslashLine(lineCurrent, styler) && !IsBackslashLine(lineCurrent - 1, styler)) {
-			levelNext++;
+		if (atEOL) {
+			levelNext += IsBackslashLine(lineCurrent, styler) - IsBackslashLine(lineCurrent - 1, styler);
 		}
-		if (atEOL && !IsBackslashLine(lineCurrent, styler) && IsBackslashLine(lineCurrent - 1, styler)) {
-			levelNext--;
-		}
-
 		if (atEOL && IsEquLine(lineCurrent, styler)) {
-			if (!IsEquLine(lineCurrent - 1, styler) && IsEquLine(lineCurrent + 1, styler))
-				levelNext++;
-			else if (IsEquLine(lineCurrent - 1, styler) && !IsEquLine(lineCurrent + 1, styler))
-				levelNext--;
+			levelNext += IsEquLine(lineCurrent + 1, styler) - IsEquLine(lineCurrent - 1, styler);
 		}
 		if (style == SCE_ASM_OPERATOR) {
 			if (ch == '{') {

scintilla/lexers/LexBash.cxx
@@ -833,10 +833,7 @@ static void FoldBashDoc(Sci_PositionU startPos, Sci_Position length, int, LexerW
 		const bool atEOL = (ch == '\r' && chNext != '\n') || (ch == '\n');
 		// Comment folding
 		if (foldComment && atEOL && IsCommentLine(lineCurrent)) {
-			if (!IsCommentLine(lineCurrent - 1) && IsCommentLine(lineCurrent + 1))
-				levelCurrent++;
-			else if (IsCommentLine(lineCurrent - 1) && !IsCommentLine(lineCurrent + 1))
-				levelCurrent--;
+			levelCurrent += IsCommentLine(lineCurrent + 1) - IsCommentLine(lineCurrent - 1);
 		}
 
 		if (style == SCE_SH_WORD) {

scintilla/lexers/LexCIL.cxx
@@ -184,10 +184,7 @@ static void FoldCILDoc(Sci_PositionU startPos, Sci_Position length, int initStyl
 		const bool atEOL = (ch == '\r' && chNext != '\n') || (ch == '\n');
 
 		if (foldComment && atEOL && IsCommentLine(lineCurrent)) {
-			if (!IsCommentLine(lineCurrent - 1) && IsCommentLine(lineCurrent + 1))
-				levelNext++;
-			else if (IsCommentLine(lineCurrent - 1) && !IsCommentLine(lineCurrent + 1))
-				levelNext--;
+			levelNext += IsCommentLine(lineCurrent + 1) - IsCommentLine(lineCurrent - 1);
 		}
 		if (foldComment && IsStreamCommentStyle(style) && !IsCommentLine(lineCurrent)) {
 			if (!IsStreamCommentStyle(stylePrev)) {

scintilla/lexers/LexCMake.cxx
@@ -313,8 +313,12 @@ void ColouriseCmakeDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initS
 	sc.Complete();
 }
 
+constexpr int GetLineCommentState(int lineState) noexcept {
+	return (lineState >> 16) & 1;
+}
+
 void FoldCmakeDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initStyle, LexerWordList, Accessor &styler) {
-	const bool foldComment = styler.GetPropertyInt("fold.comment", 1) != 0;
+	const int foldComment = styler.GetPropertyInt("fold.comment", 1);
 
 	const Sci_PositionU endPos = startPos + lengthDoc;
 	Sci_Position lineCurrent = styler.GetLine(startPos);
@@ -322,11 +326,11 @@ void FoldCmakeDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initStyle,
 	int lineCommentPrev = 0;
 	if (lineCurrent > 0) {
 		levelCurrent = styler.LevelAt(lineCurrent - 1) >> 16;
-		lineCommentPrev = styler.GetLineState(lineCurrent - 1) & CMakeLineStateMaskLineComment;
+		lineCommentPrev = GetLineCommentState(styler.GetLineState(lineCurrent - 1));
 	}
 
 	int levelNext = levelCurrent;
-	int lineCommentCurrent = styler.GetLineState(lineCurrent) & CMakeLineStateMaskLineComment;
+	int lineCommentCurrent = GetLineCommentState(styler.GetLineState(lineCurrent));
 	Sci_PositionU lineStartNext = styler.LineStart(lineCurrent + 1);
 	Sci_PositionU lineEndPos = ((lineStartNext < endPos) ? lineStartNext : endPos) - 1;
 
@@ -374,13 +378,9 @@ void FoldCmakeDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initStyle,
 		}
 
 		if (i == lineEndPos) {
-			const int lineCommentNext = styler.GetLineState(lineCurrent + 1) & CMakeLineStateMaskLineComment;
-			if (foldComment && lineCommentCurrent) {
-				if (!lineCommentPrev && lineCommentNext) {
-					levelNext++;
-				} else if (lineCommentPrev && !lineCommentNext) {
-					levelNext--;
-				}
+			const int lineCommentNext = GetLineCommentState(styler.GetLineState(lineCurrent + 1));
+			if (foldComment & lineCommentCurrent) {
+				levelNext += lineCommentNext - lineCommentPrev;
 			}
 
 			const int levelUse = levelCurrent;

scintilla/lexers/LexConfig.cxx
@@ -225,10 +225,7 @@ static void FoldConfDoc(Sci_PositionU startPos, Sci_Position length, int /*initS
 		const bool atEOL = (ch == '\r' && chNext != '\n') || (ch == '\n');
 
 		if (foldComment && atEOL && IsCommentLine(lineCurrent)) {
-			if (!IsCommentLine(lineCurrent - 1) && IsCommentLine(lineCurrent + 1))
-				levelNext++;
-			else if (IsCommentLine(lineCurrent - 1) && !IsCommentLine(lineCurrent + 1))
-				levelNext--;
+			levelNext += IsCommentLine(lineCurrent + 1) - IsCommentLine(lineCurrent - 1);
 		}
 
 		if (style == SCE_CONF_DIRECTIVE) {

scintilla/lexers/LexFSharp.cxx
@@ -216,17 +216,11 @@ static void FoldFSharpDoc(Sci_PositionU startPos, Sci_Position length, int initS
 			}
 		}
 		if (foldComment && atEOL && IsCommentLine(lineCurrent)) {
-			if (!IsCommentLine(lineCurrent - 1) && IsCommentLine(lineCurrent + 1))
-				levelNext++;
-			else if (IsCommentLine(lineCurrent - 1) && !IsCommentLine(lineCurrent + 1))
-				levelNext--;
+			levelNext += IsCommentLine(lineCurrent + 1) - IsCommentLine(lineCurrent - 1);
 		}
 
 		if (atEOL && IsOpenLine(lineCurrent)) {
-			if (!IsOpenLine(lineCurrent - 1) && IsOpenLine(lineCurrent + 1))
-				levelNext++;
-			else if (IsOpenLine(lineCurrent - 1) && !IsOpenLine(lineCurrent + 1))
-				levelNext--;
+			levelNext += IsOpenLine(lineCurrent + 1) - IsOpenLine(lineCurrent - 1);
 		}
 
 		if (foldPreprocessor && styleNext == SCE_FSHARP_PREPROCESSOR) {

scintilla/lexers/LexFortran.cxx
@@ -180,10 +180,7 @@ static void FoldFortranDoc(Sci_PositionU startPos, Sci_Position length, int /*in
 		const bool atEOL = (ch == '\r' && chNext != '\n') || (ch == '\n');
 
 		if (foldComment && atEOL && IsCommentLine(lineCurrent)) {
-			if (!IsCommentLine(lineCurrent - 1) && IsCommentLine(lineCurrent + 1))
-				levelNext++;
-			else if (IsCommentLine(lineCurrent - 1) && !IsCommentLine(lineCurrent + 1))
-				levelNext--;
+			levelNext += IsCommentLine(lineCurrent + 1) - IsCommentLine(lineCurrent - 1);
 		}
 
 		//if (style == SCE_F_WORD && stylePrev != SCE_F_WORD) {

commit 9fcec0bafd6acfa554f82de79f273b68af899e85

Fix C/C++ code folding for comment lines broken by commit 0ae4308b54728d4a9dc0536fb9f88106aaa354ba.

scintilla/lexers/LexCPP.cxx
@@ -1235,6 +1235,7 @@ static void FoldCppDoc(Sci_PositionU startPos, Sci_Position length, int initStyl
 	int style = initStyle;
 	int styleNext = styler.StyleAt(startPos);
 	bool isObjCProtocol = false;
+	bool lineCommentCurrent = IsCommentLine(lineCurrent);
 
 	for (Sci_PositionU i = startPos; i < endPos; i++) {
 		const char ch = chNext;
@@ -1244,21 +1245,26 @@ static void FoldCppDoc(Sci_PositionU startPos, Sci_Position length, int initStyl
 		styleNext = styler.StyleAt(i + 1);
 		const bool atEOL = (ch == '\r' && chNext != '\n') || (ch == '\n');
 
-		if (foldComment && atEOL && IsCommentLine(lineCurrent)) {
-			levelNext += IsCommentLine(lineCurrent + 1) - IsCommentLine(lineCurrent - 1);
-		}
-		if (foldComment && IsStreamCommentStyle(style)) {
-			if (!IsStreamCommentStyle(stylePrev)) {
-				levelNext++;
-			} else if (!IsStreamCommentStyle(styleNext) && !atEOL) {
-				levelNext--;
+		if (foldComment) {
+			if (lineCommentCurrent) {
+				if (atEOL) {
+					levelNext += IsCommentLine(lineCurrent + 1) - IsCommentLine(lineCurrent - 1);
+				}
 			}
-		}
-		if (lexType == LEX_D && foldComment && style == SCE_C_DNESTEDCOMMENT) {
-			if (ch == '/' && chNext == '+')
-				levelNext++;
-			else if (ch == '+' && chNext == '/')
-				levelNext--;
+			else if (IsStreamCommentStyle(style)) {
+				if (!IsStreamCommentStyle(stylePrev)) {
+					levelNext++;
+				} else if (!IsStreamCommentStyle(styleNext) && !atEOL) {
+					levelNext--;
+				}
+			}
+			else if (lexType == LEX_D && style == SCE_C_DNESTEDCOMMENT) {
+				if (ch == '/' && chNext == '+') {
+					levelNext++;
+				} else if (ch == '+' && chNext == '/') {
+					levelNext--;
+				}
+			}
 		}
 		if (lexType == LEX_PHP && IsHear_NowDocStyle(style)) {
 			if (!IsHear_NowDocStyle(stylePrev)) {
@@ -1398,6 +1404,7 @@ static void FoldCppDoc(Sci_PositionU startPos, Sci_Position length, int initStyl
 			//levelMinCurrent = levelCurrent;
 			visibleChars = 0;
 			isObjCProtocol = false;
+			lineCommentCurrent = IsCommentLine(lineCurrent);
 		}
 	}
 }